9f138e0805276a70c083e4b0c833e4ba179bd093
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-10-24 20:50:06 +0700

    refactor: implement new pages

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 
 import App from "@/App.vue";
-import router from "@/router";
+import router from "@/main.routes";
 import "@/main.scss";
 
 import { FontAwesomeIcon } from "@/shared/plugins/font-awesome.plugin";
