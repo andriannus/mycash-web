e4ba234a810195c298edff781c9f02685720760e
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-10-24 11:07:35 +0700

    feat: install and setup font-awesome

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import "./main.scss";
+
+import App from "@/App.vue";
+import router from "@/router";
+import "@/main.scss";
+
+import { FontAwesomeIcon } from "@/shared/plugins/font-awesome.plugin";
 
 createApp(App)
   .use(router)
+  .component("my-icon", FontAwesomeIcon)
   .mount("#app");
---
+++ src/shared/plugins/font-awesome.plugin.js
@@ -0,0 +1,9 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+const icons = [faArrowLeft];
+
+library.add(icons);
+
+export { FontAwesomeIcon };
