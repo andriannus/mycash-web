6fb0ac072e994ff825eb8a39a5c6907937651215
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-10-25 23:52:15 +0700

    refactor: implement radio component on loan purpose page

--- src/app/loan/loan-purpose/LoanPurpose.vue
+++ src/app/loan/loan-purpose/LoanPurpose.vue
@@ -7,20 +7,30 @@
   </my-header>
 
   <my-content>
-    <div>Hello World</div>
+    <my-radio-group
+      v-model="loanPurpose"
+      id="RbLoanPurpose"
+      :options="options"
+    ></my-radio-group>
 
     <template #contentFooter>
-      <button class="Button Button--primary Button--fullWidth">
+      <my-button color="primary" full-width>
         Lanjutkan
-      </button>
+      </my-button>
     </template>
   </my-content>
 </template>
 
 <script>
+import { ref } from "vue";
+
+import { LOAN_PURPOSE_OPTIONS } from "../loan.constant";
+
 import MyBackButton from "@/shared/components/my-back-button/MyBackButton.vue";
+import MyButton from "@/shared/components/my-button/MyButton.vue";
 import MyContent from "@/shared/components/my-content/MyContent.vue";
 import MyHeader from "@/shared/components/my-header/MyHeader.vue";
+import MyRadioGroup from "@/shared/components/my-radio-group/MyRadioGroup.vue";
 import MyTitle from "@/shared/components/my-title/MyTitle.vue";
 import MyToolbar from "@/shared/components/my-toolbar/MyToolbar.vue";
 
@@ -29,10 +39,19 @@ export default {
 
   components: {
     MyBackButton,
+    MyButton,
     MyContent,
     MyHeader,
+    MyRadioGroup,
     MyTitle,
     MyToolbar,
+  },
+
+  setup() {
+    const loanPurpose = ref("");
+    const options = LOAN_PURPOSE_OPTIONS;
+
+    return { loanPurpose, options };
   },
 };
 </script>
---
+++ src/app/loan/loan.constant.js
@@ -0,0 +1,42 @@
+export const LOAN_PURPOSE_OPTIONS = [
+  {
+    text: "Liburan",
+    value: "Holiday",
+  },
+  {
+    text: "KPR",
+    value: "HousingLoan",
+  },
+  {
+    text: "Mobil atau Motor",
+    value: "CarOrMotorcycle",
+  },
+  {
+    text: "Kartu Kredit",
+    value: "CreditCard",
+  },
+  {
+    text: "Renovasi",
+    value: "Renovation",
+  },
+  {
+    text: "KTA Elektronik",
+    value: "ElectronicUnsecuredLoan",
+  },
+  {
+    text: "Sekolah",
+    value: "School",
+  },
+  {
+    text: "Modal Usaha",
+    value: "VentureCapital",
+  },
+  {
+    text: "Investasi",
+    value: "Investment",
+  },
+  {
+    text: "Lainnya",
+    value: "Other",
+  },
+];
