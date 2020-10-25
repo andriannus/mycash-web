d278d23b4f7f233ef1e704e200b4dc1b51f85a83
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-10-25 23:51:20 +0700

    feat: create radio components

---
+++ src/shared/components/my-radio-group/MyRadioGroup.vue
@@ -0,0 +1,50 @@
+<template>
+  <div
+    v-for="(option, index) in options"
+    :key="index"
+    class="Radio MarginBottom-base"
+  >
+    <input
+      :id="id + index"
+      class="Radio-control"
+      :name="id"
+      :value="option.value"
+      type="radio"
+      @change="onChange"
+    />
+
+    <label class="Radio-label" :for="id + index">
+      {{ option.text }}
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  name: "MyRadioGroup",
+
+  props: {
+    id: {
+      type: String,
+      default: "",
+    },
+    options: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+    value: {
+      type: String,
+      default: "",
+    },
+  },
+
+  setup(_, { emit }) {
+    const onChange = $event => emit("input", $event.target.value);
+
+    return { onChange };
+  },
+};
+</script>
---
+++ src/shared/components/my-radio/MyRadio.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="Radio">
+    <input
+      :id="id"
+      class="Radio-control"
+      type="radio"
+      :value="value"
+      @change="onChange"
+    />
+
+    <label class="Radio-label" :for="id">
+      <slot></slot>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyRadio",
+
+  props: {
+    id: {
+      type: String,
+      default: "",
+    },
+    value: {
+      type: String,
+      default: "",
+    },
+  },
+
+  setup(_, { emit }) {
+    const onChange = $event => emit("input", $event.target.value);
+
+    return { onChange };
+  },
+};
+</script>
