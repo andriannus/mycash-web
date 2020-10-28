a1b9b58765465665d37e19b369676020fbd13657
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-10-28 22:41:21 +0700

    feat: create my-page component

---
+++ src/shared/components/my-page/MyPage.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="MyPage">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import { onMounted, onUnmounted } from "vue";
+
+export default {
+  name: "MyPage",
+
+  setup() {
+    const body = document.getElementById("app");
+
+    const addBgColorToApp = () => {
+      body.classList.add("BgColor-light");
+    };
+
+    const removeBgColorToApp = () => {
+      body.classList.remove("BgColor-light");
+    };
+
+    onMounted(() => {
+      addBgColorToApp();
+    });
+
+    onUnmounted(() => {
+      removeBgColorToApp();
+    });
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.MyPage {
+  background-color: #ffffff;
+  margin: auto;
+  max-width: 480px;
+}
+</style>
