c8672de3d5038598c49ce5b64cbe5789b0d91150
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-10-29 11:18:47 +0700

    chore: install and setup svg-loader

--- vue.config.js
+++ vue.config.js
@@ -8,4 +8,23 @@ module.exports = {
       },
     },
   },
+  chainWebpack: config => {
+    const svgRule = config.module.rule("svg");
+
+    svgRule.uses.clear();
+
+    svgRule
+      .use("vue-loader")
+      .loader("vue-loader-v16")
+      .end()
+      .use("vue-svg-loader")
+      .loader("vue-svg-loader");
+
+    config.plugin("html").tap(args => {
+      const [arg] = args;
+      arg.title = "MyCash Mini";
+
+      return args;
+    });
+  },
 };
