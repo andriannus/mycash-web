b081469b0672c09abf3a49cbc6641b656b5ae6e5
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-10-30 21:00:10 +0700

    feat: create fade-transition component

---
+++ src/shared/transitions/MyFadeTransition.vue
@@ -0,0 +1,25 @@
+<template>
+  <transition name="fade">
+    <slot></slot>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "MyFadeTransition",
+};
+</script>
+
+<style lang="scss" scoped>
+.fade {
+  &-enter-active,
+  &-leave-active {
+    transition: opacity 0.3s;
+  }
+
+  &-enter,
+  &-leave-to {
+    opacity: 0;
+  }
+}
+</style>
