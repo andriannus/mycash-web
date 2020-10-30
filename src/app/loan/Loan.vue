290d4797742062ef8db82130dabef6bcfe76c015
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-10-30 21:00:58 +0700

    refactor: implement fade-transition on Loan Page

--- src/app/loan/Loan.vue
+++ src/app/loan/Loan.vue
@@ -1,3 +1,19 @@
 <template>
-  <router-view></router-view>
+  <router-view v-slot="{ Component }">
+    <my-fade-transition>
+      <component :is="Component"></component>
+    </my-fade-transition>
+  </router-view>
 </template>
+
+<script>
+import MyFadeTransition from "@/shared/transitions/MyFadeTransition.vue";
+
+export default {
+  name: "Loan",
+
+  components: {
+    MyFadeTransition,
+  },
+};
+</script>
