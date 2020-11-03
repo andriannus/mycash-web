7a937800107aebe75844dfbb8922f28651915c80
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-11-04 00:06:27 +0700

    feat: create top-bar component

---
+++ src/shared/components/my-top-bar/MyTopBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="TopBar">
+    <router-link class="TopBar-logo" to="/">
+      <tunaiku-icon></tunaiku-icon>
+    </router-link>
+  </div>
+</template>
+
+<script>
+import TunaikuIcon from "@/shared/images/svg/tunaiku-logo.svg";
+
+export default {
+  name: "MyTopBar",
+
+  components: {
+    TunaikuIcon,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.TopBar {
+  position: sticky;
+  top: 0;
+  z-index: 10;
+}
+</style>
