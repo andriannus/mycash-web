94f4ecb8c811f7deb5c626e9b30b23745faadff6
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-11-06 21:28:43 +0700

    feat: create landing-terms-loan component

---
+++ src/app/landing/shared/components/landing-terms-loan/LandingTermsLoan.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="BgColor-info PaddingTop-xlarge PaddingBottom-xlarge">
+    <div class="Container">
+      <div class="TextAlign-center MarginBottom-large">
+        <p class="FontSize-large">
+          Syarat Mengajukan Pinjaman
+        </p>
+      </div>
+
+      <div class="Grids">
+        <div class="LandingTermsLoan-illustration Grid TextAlign-center">
+          <terms-illustration-icon></terms-illustration-icon>
+        </div>
+
+        <div class="Grid Flex AlignItems-center JustifyContent-center">
+          <ul class="LandingTermsLoan-list">
+            <li
+              v-for="term in terms"
+              :key="term"
+              class="Flex AlignItems-center"
+            >
+              <my-icon
+                class="MarginRight-base"
+                :icon="['far', 'check-circle']"
+                size="lg"
+              ></my-icon>
+
+              {{ term }}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { APPLYING_LOAN_TERMS } from "./landing-terms-loan.constant";
+import TermsIllustrationIcon from "../../images/terms-illustration.svg";
+
+export default {
+  name: "LandingTermsLoan",
+
+  components: {
+    TermsIllustrationIcon,
+  },
+
+  setup() {
+    const terms = APPLYING_LOAN_TERMS;
+
+    return { terms };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@amar-ui-web/responsive/scss/mixins";
+
+.LandingTermsLoan {
+  &-list {
+    li {
+      &:not(:last-child) {
+        margin-bottom: 1rem;
+      }
+    }
+  }
+
+  @include amb-responsive-media("xs") {
+    text-align: center;
+
+    &-illustration {
+      display: none;
+    }
+  }
+
+  @include amb-responsive-media("md") {
+    &-illustration {
+      display: block;
+    }
+  }
+}
+</style>
---
+++ src/app/landing/shared/components/landing-terms-loan/landing-terms-loan.constant.js
@@ -0,0 +1,7 @@
+export const APPLYING_LOAN_TERMS = [
+  "Usia 21 sampai 55 tahun",
+  "Warga Negara Indonesia (WNI)",
+  "Memiliki rekening bank",
+  "Mempunyai pekerjaan atau penghasilan",
+  "Alamat KTP atau berdomisili dalam area layanan kami",
+];
