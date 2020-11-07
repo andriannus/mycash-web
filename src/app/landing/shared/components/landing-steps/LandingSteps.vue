0e0d5cc66bb4460b50d168adc626bdc13721c3a7
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-11-07 08:15:15 +0700

    feat: create landing-steps component

---
+++ src/app/landing/shared/components/landing-steps/LandingSteps.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="Container PaddingTop-xlarge PaddingBottom-xlarge">
+    <div class="TextAlign-center MarginBottom-large">
+      <p class="FontSize-large">
+        Langkah Mengajukan Pinjaman di Tunaiku
+      </p>
+    </div>
+
+    <div class="Grids Flex AlignItems-center">
+      <div class="Grid">
+        <div
+          v-for="(step, index) in steps"
+          :key="index"
+          class="LandingSteps-list"
+        >
+          <div class="Flex">
+            <span class="LandingSteps-number Color-info">
+              {{ index + 1 }}
+            </span>
+
+            <p>
+              <strong>{{ step.title }}</strong>
+            </p>
+          </div>
+
+          <div class="MarginLeft-large TextAlign-justify">
+            <span class="FontSize-small Color-light-80">
+              {{ step.description }}
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div class="LandingSteps-illustration Grid">
+        <steps-illustration-icon></steps-illustration-icon>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { APPLY_LOAN_STEPS } from "./landing-steps.constant";
+import StepsIllustrationIcon from "../../images/steps-illustration.svg";
+
+export default {
+  name: "LandingSteps",
+
+  components: {
+    StepsIllustrationIcon,
+  },
+
+  setup() {
+    const steps = APPLY_LOAN_STEPS;
+
+    return { steps };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@amar-ui-web/color/scss/variables";
+@import "@amar-ui-web/responsive/scss/mixins";
+
+.LandingSteps {
+  &-list {
+    &:not(:last-child) {
+      margin-bottom: 1rem;
+    }
+  }
+
+  &-number {
+    border: 1px solid $amb-info;
+    border-radius: 50%;
+    height: 1.75rem;
+    margin-right: 0.5rem;
+    text-align: center;
+    width: 1.75rem;
+  }
+
+  @include amb-responsive-media("xs") {
+    &-illustration {
+      display: none;
+    }
+
+    &-list {
+      margin-right: 0;
+    }
+  }
+
+  @include amb-responsive-media("md") {
+    &-illustration {
+      align-items: center;
+      display: flex;
+      justify-content: center;
+    }
+
+    &-list {
+      margin-right: 1rem;
+    }
+  }
+}
+</style>
---
+++ src/app/landing/shared/components/landing-steps/landing-steps.constant.js
@@ -0,0 +1,26 @@
+export const APPLY_LOAN_STEPS = [
+  {
+    title: "Pendaftaran",
+    description:
+      "Pilih jumlah dan lama pinjaman yang Anda inginkan. Isi formulir pengajuan kirimkan formulir pengajuan.",
+  },
+  {
+    title: "Terima Persetujuan",
+    description:
+      "Data Anda akan diproses oleh tim analis kami. Silakan tunggu kurang lebih 24 jam untuk mengetahui status pengajuan Anda.",
+  },
+  {
+    title: "Konfirmasi Pinjaman",
+    description:
+      "Konfirmasi pinjaman Anda yang telah disetujui dan atur lokasi tanda tangan kontrak Anda dengan kurir kami.",
+  },
+  {
+    title: "Tanda Tangan Kontrak",
+    description:
+      "Kurir kami akan membawa kontrak asli untuk Anda tandatangani sebagai bukti perjanjian, dalam 1-3 hari kerja (Tunaiku tidak melakukan survei).",
+  },
+  {
+    title: "Terima Dana",
+    description: "Dana akan dicairkan ke rekening bank Anda (1-3 hari kerja).",
+  },
+];
