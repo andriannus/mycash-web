1d59e91eb72ef396f1cf3e410ada4695812c2503
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-11-22 22:01:33 +0700

    feat: create slide-down transition

---
+++ src/shared/transitions/MySlideDownTransition.vue
@@ -0,0 +1,26 @@
+<template>
+  <transition name="SlideDown">
+    <slot></slot>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "MySlideDownTransition",
+};
+</script>
+
+<style lang="scss" scoped>
+.SlideDown {
+  &-enter-active,
+  &-leave-active {
+    transition: all 0.3s ease-in-out;
+  }
+
+  &-enter-from,
+  &-leave-to {
+    transform: translateY(-100px);
+    opacity: 0;
+  }
+}
+</style>
