785945423fbaef8c4b253e76c7f8d7b768411b42
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-11-28 11:34:26 +0700

    feat: create bounce transition

---
+++ src/shared/transitions/MyBounceTransition.vue
@@ -0,0 +1,37 @@
+<template>
+  <transition name="Bounce">
+    <slot></slot>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "MyBounceTransition",
+};
+</script>
+
+<style lang="scss" scoped>
+.Bounce {
+  &-enter-active {
+    animation: BounceIn 0.3s;
+  }
+
+  &-leave-active {
+    animation: BounceIn 0.3s reverse;
+  }
+}
+
+@keyframes BounceIn {
+  0% {
+    transform: scale(0);
+  }
+
+  50% {
+    transform: scale(1.5);
+  }
+
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
