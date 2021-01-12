a1de87288f73bfd99ccd95c7c9eab93aee03b4bd
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-01-12 15:19:05 +0700

    refactor: adjust about-us page

---
+++ src/app/general/about-us/about-us.component.js
@@ -0,0 +1,20 @@
+import GeneralFooter from "../shared/components/general-footer/GeneralFooter.vue";
+import EasyAndFastIcon from "../shared/images/easy-and-fast.svg";
+
+import AppApplyLoanButton from "@/shared/components/apply-loan-button/apply-loan-button.component.vue";
+import MyContent from "@/shared/components/my-content/MyContent.vue";
+import MyPage from "@/shared/components/my-page/MyPage.vue";
+import MyTopBar from "@/shared/components/my-top-bar/MyTopBar.vue";
+
+export default {
+  name: "AboutUs",
+
+  components: {
+    AppApplyLoanButton,
+    EasyAndFastIcon,
+    GeneralFooter,
+    MyContent,
+    MyPage,
+    MyTopBar,
+  },
+};
