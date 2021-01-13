0cec1fd4b3351df3671c5267f6365290d455dffc
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-01-13 19:22:38 +0700

    refactor: implement new radio component

--- src/app/loan/loan-confirm-area/LoanConfirmArea.vue
+++ src/app/loan/loan-confirm-area/LoanConfirmArea.vue
@@ -81,7 +81,7 @@ import MyButton from "@/shared/components/button/button.component.vue";
 import MyContent from "@/shared/components/content/content.component.vue";
 import MyHeader from "@/shared/components/header/header.component.vue";
 import MyPage from "@/shared/components/my-page/MyPage.vue";
-import MyRadio from "@/shared/components/my-radio/MyRadio.vue";
+import MyRadio from "@/shared/components/radio/radio.component.vue";
 import MyTitle from "@/shared/components/my-title/MyTitle.vue";
 import MyToolbar from "@/shared/components/my-toolbar/MyToolbar.vue";
 
