8d96ad338b9e4e20b1d331316d1733f9e204c38a
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-01-13 20:04:40 +0700

    refactor: adjust radio-group component

---
+++ src/shared/components/radio-group/radio-group.component.js
@@ -0,0 +1,29 @@
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "RadioGroup",
+  inheritAttrs: false,
+
+  props: {
+    id: {
+      type: String,
+      default: "RbGroup",
+    },
+    options: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+    modelValue: {
+      type: String,
+      default: null,
+    },
+  },
+
+  setup(_, { emit }) {
+    const onChange = $event => emit("update:modelValue", $event.target.value);
+
+    return { onChange };
+  },
+});
---
+++ src/shared/components/radio-group/radio-group.component.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    v-for="(option, index) in options"
+    :key="index"
+    class="Radio MarginBottom-base"
+  >
+    <input
+      :id="id + index"
+      class="Radio-control"
+      :name="id"
+      :value="option.value"
+      type="radio"
+      @change="onChange"
+    />
+
+    <label class="Radio-label" :for="id + index">
+      {{ option.text }}
+    </label>
+  </div>
+</template>
+
+<script src="./radio-group.component.js"></script>
