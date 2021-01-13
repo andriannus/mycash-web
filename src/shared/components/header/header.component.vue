935c42c0133c47f100ed408adb4bda69c2594f94
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-01-13 18:42:11 +0700

    refactor: adjust header component

---
+++ src/shared/components/header/header.component.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="Header">
+    <slot></slot>
+  </div>
+</template>
+
+<script src="./header.component.js"></script>
+<style lang="scss" scoped src="./header.component.scss"></style>
