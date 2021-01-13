53e3103cdb80d2071fbfbf2220d7d5d58da8a522
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-01-13 20:43:37 +0700

    refactor: adjust title component

---
+++ src/shared/components/title/title.component.vue
@@ -0,0 +1,8 @@
+<template>
+  <span class="MyTitle">
+    <slot></slot>
+  </span>
+</template>
+
+<script src="./title.component.js"></script>
+<style lang="scss" scoped src="./title.component.scss"></style>
