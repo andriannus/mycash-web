579a554718dba33544268d28a38e43af1986b2aa
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-01-13 20:38:34 +0700

    refactor: adjust top-bar component

---
+++ src/shared/components/top-bar/top-bar.component.js
@@ -0,0 +1,11 @@
+import { defineComponent } from "vue";
+
+import TunaikuIcon from "@/shared/images/svg/tunaiku-logo.svg";
+
+export default defineComponent({
+  name: "TopBar",
+
+  components: {
+    TunaikuIcon,
+  },
+});
---
+++ src/shared/components/top-bar/top-bar.component.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="TopBar">
+    <router-link class="TopBar-logo" to="/">
+      <tunaiku-icon></tunaiku-icon>
+    </router-link>
+  </div>
+</template>
+
+<script src="./top-bar.component.js"></script>
+<style lang="scss" scoped src="./top-bar.component.scss"></style>
