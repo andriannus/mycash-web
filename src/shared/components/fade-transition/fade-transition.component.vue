fcf603eeca146f5a7255840157f2a23e548e9b2f
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-01-14 18:52:41 +0700

    refactor: adjust fade-transition component

---
+++ src/shared/components/fade-transition/fade-transition.component.vue
@@ -0,0 +1,8 @@
+<template>
+  <transition name="Fade">
+    <slot></slot>
+  </transition>
+</template>
+
+<script src="./fade-transition.component.js"></script>
+<style lang="scss" scoped src="./fade-transition.component.scss"></style>
