b90ec7993b8eb12a5acca437c86629970b2b8d8e
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-01-14 20:56:00 +0700

    refactor: adjust loan-ktp-data page

--- src/app/loan/loan-ktp-data/loan-ktp-data.component.js
+++ src/app/loan/loan-ktp-data/loan-ktp-data.component.js
@@ -1,65 +1,4 @@
-<template>
-  <my-page>
-    <my-header>
-      <my-toolbar>
-        <my-back-button v-if="setup.isBackButtonShown"></my-back-button>
-        <my-title>Data KTP</my-title>
-      </my-toolbar>
-    </my-header>
-
-    <my-content>
-      <template #progress>
-        <loan-progress :max="100" :value="pagePercentage"></loan-progress>
-      </template>
-
-      <p class="FontSize-small MarginBottom">
-        Lengkapi semua data di bawah ini sesuai dengan KTP Anda.
-      </p>
-
-      <my-text-field
-        id="TxtKtp"
-        :disabled="true"
-        label="Nomor KTP (NIK)"
-        placeholder="Masukkan nomor KTP kamu"
-      ></my-text-field>
-
-      <my-text-field
-        id="TxtFullName"
-        :disabled="true"
-        label="Nama Sesuai KTP"
-        placeholder="Masukkan nama kamu"
-      ></my-text-field>
-
-      <div class="Field">
-        <label class="Field-label">Jenis Kelamin</label>
-
-        <div class="Field-control Flex">
-          <my-radio
-            id="RbMale"
-            className="MarginRight"
-            name="Gender"
-            value="male"
-          >
-            Laki-laki
-          </my-radio>
-
-          <my-radio id="RbFemale" name="Gender" value="female">
-            Perempuan
-          </my-radio>
-        </div>
-      </div>
-
-      <template #contentFooter>
-        <my-button color="primary" full-width @click="onClickNextButton">
-          Lanjutkan
-        </my-button>
-      </template>
-    </my-content>
-  </my-page>
-</template>
-
-<script>
-import { computed, reactive } from "vue";
+import { computed, defineComponent, reactive } from "vue";
 import { useRoute, useRouter } from "vue-router";
 
 import { LOAN_PAGE_PATH, LOAN_PAGES } from "../loan.constant";
@@ -76,7 +15,7 @@ import MyTextField from "@/shared/components/text-field/text-field.component.vue
 import MyTitle from "@/shared/components/title/title.component.vue";
 import MyToolbar from "@/shared/components/toolbar/toolbar.component.vue";
 
-export default {
+export default defineComponent({
   name: "LoanKtpData",
 
   components: {
@@ -115,5 +54,4 @@ export default {
       setup,
     };
   },
-};
-</script>
+});
---
+++ src/app/loan/loan-ktp-data/loan-ktp-data.component.vue
@@ -0,0 +1,61 @@
+<template>
+  <my-page>
+    <my-header>
+      <my-toolbar>
+        <my-back-button v-if="setup.isBackButtonShown"></my-back-button>
+        <my-title>Data KTP</my-title>
+      </my-toolbar>
+    </my-header>
+
+    <my-content>
+      <template #progress>
+        <loan-progress :max="100" :value="pagePercentage"></loan-progress>
+      </template>
+
+      <p class="FontSize-small MarginBottom">
+        Lengkapi semua data di bawah ini sesuai dengan KTP Anda.
+      </p>
+
+      <my-text-field
+        id="TxtKtp"
+        :disabled="true"
+        label="Nomor KTP (NIK)"
+        placeholder="Masukkan nomor KTP kamu"
+      ></my-text-field>
+
+      <my-text-field
+        id="TxtFullName"
+        :disabled="true"
+        label="Nama Sesuai KTP"
+        placeholder="Masukkan nama kamu"
+      ></my-text-field>
+
+      <div class="Field">
+        <label class="Field-label">Jenis Kelamin</label>
+
+        <div class="Field-control Flex">
+          <my-radio
+            id="RbMale"
+            className="MarginRight"
+            name="Gender"
+            value="male"
+          >
+            Laki-laki
+          </my-radio>
+
+          <my-radio id="RbFemale" name="Gender" value="female">
+            Perempuan
+          </my-radio>
+        </div>
+      </div>
+
+      <template #contentFooter>
+        <my-button color="primary" full-width @click="onClickNextButton">
+          Lanjutkan
+        </my-button>
+      </template>
+    </my-content>
+  </my-page>
+</template>
+
+<script src="./loan-ktp-data.component.js"></script>
