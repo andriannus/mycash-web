4b8a002052ad90a47aac5a638058c8efed9cb762
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-01-14 18:49:05 +0700

    refactor: adjust bounce-transition component

---
+++ src/shared/components/bounce-transition/bounce-transition.component.vue
@@ -0,0 +1,8 @@
+<template>
+  <transition name="Bounce">
+    <slot></slot>
+  </transition>
+</template>
+
+<script src="./bounce-transition.component.js"></script>
+<style lang="scss" scoped src="./bounce-transition.component.scss"></style>
