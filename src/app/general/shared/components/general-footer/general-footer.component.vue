7ae129f27a06cdc998d8b599a695e1d5d3b19bd0
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-01-14 20:27:23 +0700

    refactor: adjust general-footer component

--- src/app/general/shared/components/general-footer/general-footer.component.vue
+++ src/app/general/shared/components/general-footer/general-footer.component.vue
@@ -136,36 +136,4 @@
   </div>
 </template>
 
-<script>
-import {
-  OTHER_LINKS,
-  PRODUCT_LINKS,
-  SOCIAL_MEDIAS,
-  TUNAIKU_LINKS,
-} from "./general-footer.constant";
-import AmarBankIcon from "../../images/amar-bank.svg";
-import OjkIcon from "../../images/ojk.svg";
-
-export default {
-  name: "GeneralFooter",
-
-  components: {
-    AmarBankIcon,
-    OjkIcon,
-  },
-
-  setup() {
-    const otherLinks = OTHER_LINKS;
-    const productLinks = PRODUCT_LINKS;
-    const socialMedias = SOCIAL_MEDIAS;
-    const tunaikuLinks = TUNAIKU_LINKS;
-
-    return {
-      otherLinks,
-      productLinks,
-      socialMedias,
-      tunaikuLinks,
-    };
-  },
-};
-</script>
+<script src="./general-footer.component.js"></script>
