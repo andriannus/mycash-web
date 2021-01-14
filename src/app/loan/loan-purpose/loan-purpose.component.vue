51e0f6a323cb7eaa289578f2582bd4b00815e509
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-01-14 21:00:14 +0700

    refactor: adjust loan-purpose page

---
+++ src/app/loan/loan-purpose/loan-purpose.component.vue
@@ -0,0 +1,31 @@
+<template>
+  <my-page>
+    <my-header>
+      <my-toolbar>
+        <my-back-button></my-back-button>
+        <my-title>Tujuan Pinjaman</my-title>
+      </my-toolbar>
+    </my-header>
+
+    <my-content>
+      <my-radio-group
+        v-model="loanPurpose"
+        id="RbLoanPurpose"
+        :options="options"
+      ></my-radio-group>
+
+      <template #contentFooter>
+        <my-button
+          color="primary"
+          :disabled="hasNotBeenSelected"
+          full-width
+          @click="onClickNextButton"
+        >
+          Lanjutkan
+        </my-button>
+      </template>
+    </my-content>
+  </my-page>
+</template>
+
+<script src="./loan-purpose.component.js"></script>
