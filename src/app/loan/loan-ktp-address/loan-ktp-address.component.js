d5a7435e293eedfcfe7ce13feeae013ade26a630
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-01-14 20:53:46 +0700

    refactor: adjust loan-ktp-address page

--- src/app/loan/loan-ktp-address/loan-ktp-address.component.js
+++ src/app/loan/loan-ktp-address/loan-ktp-address.component.js
@@ -1,53 +1,4 @@
-<template>
-  <my-page>
-    <my-header>
-      <my-toolbar>
-        <my-back-button></my-back-button>
-        <my-title>Alamat KTP</my-title>
-      </my-toolbar>
-    </my-header>
-
-    <my-content>
-      <template #progress>
-        <loan-progress :max="100" :value="pagePercentage"></loan-progress>
-      </template>
-
-      <p class="FontSize-small MarginBottom">
-        Lengkapi semua data di bawah ini sesuai dengan KTP Anda.
-      </p>
-
-      <div class="Grids">
-        <div class="Grid">
-          <my-text-field id="TxtBlockNumber" label="Nomor Blok"></my-text-field>
-        </div>
-
-        <div class="Grid">
-          <my-text-field
-            id="TxtHouseNumber"
-            label="Nomor Rumah"
-          ></my-text-field>
-        </div>
-      </div>
-
-      <div class="Grids">
-        <div class="Grid">
-          <my-text-field id="TxtRt" label="RT"></my-text-field>
-        </div>
-
-        <div class="Grid">
-          <my-text-field id="TxtRw" label="RW"></my-text-field>
-        </div>
-      </div>
-
-      <template #contentFooter>
-        <my-button color="primary" full-width>Lanjutkan</my-button>
-      </template>
-    </my-content>
-  </my-page>
-</template>
-
-<script>
-import { computed } from "vue";
+import { computed, defineComponent } from "vue";
 import { useRoute, useRouter } from "vue-router";
 
 import { LOAN_PAGE_PATH, LOAN_PAGES } from "../loan.constant";
@@ -63,7 +14,7 @@ import MyTextField from "@/shared/components/text-field/text-field.component.vue
 import MyTitle from "@/shared/components/title/title.component.vue";
 import MyToolbar from "@/shared/components/toolbar/toolbar.component.vue";
 
-export default {
+export default defineComponent({
   name: "LoanKtpAddress",
 
   components: {
@@ -96,5 +47,4 @@ export default {
       pagePercentage,
     };
   },
-};
-</script>
+});
---
+++ src/app/loan/loan-ktp-address/loan-ktp-address.component.vue
@@ -0,0 +1,49 @@
+<template>
+  <my-page>
+    <my-header>
+      <my-toolbar>
+        <my-back-button></my-back-button>
+        <my-title>Alamat KTP</my-title>
+      </my-toolbar>
+    </my-header>
+
+    <my-content>
+      <template #progress>
+        <loan-progress :max="100" :value="pagePercentage"></loan-progress>
+      </template>
+
+      <p class="FontSize-small MarginBottom">
+        Lengkapi semua data di bawah ini sesuai dengan KTP Anda.
+      </p>
+
+      <div class="Grids">
+        <div class="Grid">
+          <my-text-field id="TxtBlockNumber" label="Nomor Blok"></my-text-field>
+        </div>
+
+        <div class="Grid">
+          <my-text-field
+            id="TxtHouseNumber"
+            label="Nomor Rumah"
+          ></my-text-field>
+        </div>
+      </div>
+
+      <div class="Grids">
+        <div class="Grid">
+          <my-text-field id="TxtRt" label="RT"></my-text-field>
+        </div>
+
+        <div class="Grid">
+          <my-text-field id="TxtRw" label="RW"></my-text-field>
+        </div>
+      </div>
+
+      <template #contentFooter>
+        <my-button color="primary" full-width>Lanjutkan</my-button>
+      </template>
+    </my-content>
+  </my-page>
+</template>
+
+<script src="./loan-ktp-address.component.js"></script>
