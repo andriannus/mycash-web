9a985db9dc46bf5a5110b84a18ee8255befe6172
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-01-15 20:17:39 +0700

    refactor: implement new child components

--- src/app/landing/landing.component.js
+++ src/app/landing/landing.component.js
@@ -1,14 +1,14 @@
 import { defineComponent, onMounted } from "vue";
 
 import { PAGE_TITLE } from "./landing.constant";
-import LandingAchieved from "./shared/components/landing-achieved/LandingAchieved.vue";
-import LandingAdvantages from "./shared/components/landing-advantages/LandingAdvantages.vue";
-import LandingCost from "./shared/components/landing-cost/LandingCost.vue";
-import LandingDownload from "./shared/components/landing-download/LandingDownload.vue";
-import LandingServiceArea from "./shared/components/landing-service-area/LandingServiceArea.vue";
-import LandingSteps from "./shared/components/landing-steps/LandingSteps.vue";
-import LandingTermsLoan from "./shared/components/landing-terms-loan/LandingTermsLoan.vue";
-import LandingTestimonial from "./shared/components/landing-testimonial/LandingTestimonial.vue";
+import LandingAchieved from "./shared/components/landing-achieved/landing-achieved.component.vue";
+import LandingAdvantages from "./shared/components/landing-advantages/landing-advantages.component.vue";
+import LandingCost from "./shared/components/landing-cost/landing-cost.component.vue";
+import LandingDownload from "./shared/components/landing-download/landing-download.component.vue";
+import LandingServiceArea from "./shared/components/landing-service-area/landing-service-area.component.vue";
+import LandingSteps from "./shared/components/landing-steps/landing-steps.component.vue";
+import LandingTermsLoan from "./shared/components/landing-terms-loan/landing-terms-loan.component.vue";
+import LandingTestimonial from "./shared/components/landing-testimonial/landing-testimonial.component.vue";
 
 import AppApplyLoanButton from "@/app/shared/components/apply-loan-button/apply-loan-button.component.vue";
 import AppLoanSlider from "@/app/shared/components/loan-slider/loan-slider.component.vue";
