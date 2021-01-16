d2355e7a72c65c8f9398999eb9c2755adbb3da9d
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-01-16 23:27:50 +0700

    feat: create feedback component

---
+++ src/app/shared/components/feedback/feedback.component.vue
@@ -0,0 +1,9 @@
+<template>
+  <button class="Feedback-button">
+    <my-icon :icon="['far', 'comment-alt']" size="xs"></my-icon>
+    <span class="Feedback-label">Saran</span>
+  </button>
+</template>
+
+<script src="./feedback.component.js"></script>
+<style lang="scss" scoped src="./feedback.component.scss"></style>
