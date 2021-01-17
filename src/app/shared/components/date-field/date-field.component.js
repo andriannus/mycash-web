0535ebbd08ef6359344ff2b95827d8365045dde6
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-01-17 23:04:57 +0700

    feat: create date-field component

---
+++ src/app/shared/components/date-field/date-field.component.js
@@ -0,0 +1,62 @@
+import { computed, defineComponent, onMounted } from "vue";
+import flatpickr from "flatpickr";
+import { Indonesian } from "flatpickr/dist/l10n/id";
+
+export default defineComponent({
+  name: "DateField",
+
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    errors: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+    id: {
+      type: String,
+      default: "",
+    },
+    label: {
+      type: String,
+      default: "",
+    },
+    modelValue: {
+      type: String,
+      default: null,
+    },
+    placeholder: {
+      type: String,
+      default: "",
+    },
+  },
+
+  setup(props, { emit }) {
+    const isErrorExist = computed(() => props.errors.length > 0);
+    const isLabelExist = computed(() => !!props.label);
+
+    onMounted(() => {
+      flatPickrInit();
+    });
+
+    const onInput = event => {
+      emit("update:modelValue", event.target.value);
+    };
+
+    const flatPickrInit = () => {
+      const options = {
+        altFormat: "j F Y",
+        altInput: true,
+        dateFormat: "Y-m-d",
+        locale: Indonesian,
+      };
+
+      flatpickr(`#${props.id}`, options);
+    };
+
+    return { isErrorExist, isLabelExist, onInput };
+  },
+});
---
+++ src/app/shared/components/date-field/date-field.component.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="Field MarginBottom">
+    <label
+      v-if="isLabelExist"
+      :class="{ 'Color-danger': isErrorExist }"
+      class="Field-label"
+      :for="id"
+    >
+      {{ label }}
+    </label>
+
+    <div class="Field-control">
+      <input
+        :id="id"
+        :class="{ 'Input--danger': isErrorExist }"
+        class="Input"
+        :disabled="disabled"
+        :placeholder="placeholder"
+        type="text"
+        :value="modelValue"
+        @input="onInput"
+      />
+    </div>
+
+    <div v-if="isErrorExist" class="Field-helper Color-danger">
+      {{ errors[0] }}
+    </div>
+  </div>
+</template>
+
+<script src="./date-field.component.js"></script>
