1a7ed2ecf41cc4fc079aee93597269a8ab43664d
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-01-17 12:15:52 +0700

    improvement: adjust feedback component

--- src/app/shared/components/feedback/feedback.component.js
+++ src/app/shared/components/feedback/feedback.component.js
@@ -1,5 +1,37 @@
-import { defineComponent } from "vue";
+import { defineComponent, reactive } from "vue";
+
+import MyBounceTransition from "@/app/shared/components/bounce-transition/bounce-transition.component.vue";
+import MyButton from "@/app/shared/components/button/button.component.vue";
+import MyCloseButton from "@/app/shared/components/close-button/close-button.component.vue";
+import MyContent from "@/app/shared/components/content/content.component.vue";
+import MyHeader from "@/app/shared/components/header/header.component.vue";
+import MyPage from "@/app/shared/components/page/page.component.vue";
+import MyTitle from "@/app/shared/components/title/title.component.vue";
+import MyToolbar from "@/app/shared/components/toolbar/toolbar.component.vue";
 
 export default defineComponent({
   name: "Feedback",
+
+  components: {
+    MyBounceTransition,
+    MyButton,
+    MyCloseButton,
+    MyContent,
+    MyHeader,
+    MyPage,
+    MyToolbar,
+    MyTitle,
+  },
+
+  setup() {
+    const state = reactive({
+      isFeedbackFormShown: false,
+    });
+
+    const toggleFeedbackForm = () => {
+      state.isFeedbackFormShown = !state.isFeedbackFormShown;
+    };
+
+    return { state, toggleFeedbackForm };
+  },
 });
--- src/app/shared/components/feedback/feedback.component.vue
+++ src/app/shared/components/feedback/feedback.component.vue
@@ -1,8 +1,63 @@
 <template>
-  <button class="Feedback-button">
+  <button class="Feedback-button" @click="toggleFeedbackForm">
     <my-icon :icon="['far', 'comment-alt']" size="xs"></my-icon>
     <span class="Feedback-label">Saran</span>
   </button>
+
+  <teleport to="body">
+    <my-bounce-transition>
+      <my-page
+        v-if="state.isFeedbackFormShown"
+        class-name="Feedback-form"
+        pop-up-window
+      >
+        <my-header>
+          <my-toolbar color="info">
+            <my-close-button @click="toggleFeedbackForm"></my-close-button>
+            <my-title>Masukan dan Saran</my-title>
+          </my-toolbar>
+        </my-header>
+
+        <my-content>
+          <p class="MarginBottom-base">
+            Bantu kami melayani Anda lebih baik.
+          </p>
+
+          <div>
+            <p>
+              <strong>Seberapa mudah pengisian formulir di Tunaiku.</strong>
+            </p>
+          </div>
+
+          <div>
+            <p>
+              <strong>
+                Apa yang Anda rasakan saat mengisi formulir pinjaman di Tunaiku?
+              </strong>
+            </p>
+
+            <p class="MarginBottom-base">
+              Pilih salah satu saja.
+            </p>
+          </div>
+
+          <div>
+            <p>
+              <strong>
+                Ceritakan alasan Anda memberikan penilaian tersebut.
+              </strong>
+            </p>
+          </div>
+
+          <template #contentFooter>
+            <my-button color="primary" full-width>
+              Kirim
+            </my-button>
+          </template>
+        </my-content>
+      </my-page>
+    </my-bounce-transition>
+  </teleport>
 </template>
 
 <script src="./feedback.component.js"></script>
